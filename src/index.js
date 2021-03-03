da533e9d4484f439473a8088afad3d3fb733fad6
Eddy Yeung <yeung.edward36@gmail.com>
2021-03-02 19:30:57 -0500

    remove hashrouter

--- src/index.js
+++ src/index.js
@@ -1,16 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { HashRouter, Route, Switch } from "react-router-dom";
+// import { HashRouter, Route, Switch } from "react-router-dom";
 
 import "@fortawesome/fontawesome-free/css/all.min.css";
 
 import Landing from "views/Lading.js";
 
-ReactDOM.render(
-  <HashRouter>
-    <Switch>
-      <Route path="/" component={Landing} />
-    </Switch>
-  </HashRouter>,
-  document.getElementById("root")
-);
+ReactDOM.render(<Landing />, document.getElementById("root"));
