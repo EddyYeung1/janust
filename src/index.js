23a92e102e21c86926de9bd3e8e5b83594d95782
Eddy Yeung <yeung.edward36@gmail.com>
2021-03-02 23:07:36 -0500

    try

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
-// import { HashRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 import "@fortawesome/fontawesome-free/css/all.min.css";
 
 import Landing from "views/Lading.js";
 
-ReactDOM.render(<Landing />, document.getElementById("root"));
+ReactDOM.render(
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <Landing />
+  </BrowserRouter>,
+  document.getElementById("root")
+);
